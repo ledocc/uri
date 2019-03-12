039112daf44cb380509be6bfbd3088130f621f59
David Callu <callu.david@gmail.com>
2019-03-12 14:19:07 +0100

    fix(parsers): parser ipv4 and ipv6 correctly

--- include/uri/parsers/rules/uri/ip.hpp
+++ include/uri/parsers/rules/uri/ip.hpp
@@ -3,6 +3,7 @@
 
 
 
+#include <iostream>
 #include <string>
 
 #include <boost/phoenix/core/reference.hpp>
@@ -48,25 +49,36 @@ public:
         //               / [                *4( h16    ":" )     h16 ]    "::"                                    ls32
         //               / [                *5( h16    ":" )     h16 ]    "::"                                    h16
         //               / [                *6( h16    ":" )     h16 ]    "::"
-        _IPv6address     = (                                                                    qi::repeat(6)[ _h16_colon ] >> _ls32 )
-                         | (                                                qi::string("::") >> qi::repeat(5)[ _h16_colon ] >> _ls32 )
-                         | ( -(                                   _h16 ) >> qi::string("::") >> qi::repeat(4)[ _h16_colon ] >> _ls32 )
-                         | ( -( qi::repeat(0, 1)[ _h16_colon ] >> _h16 ) >> qi::string("::") >> qi::repeat(3)[ _h16_colon ] >> _ls32 )
-                         | ( -( qi::repeat(0, 2)[ _h16_colon ] >> _h16 ) >> qi::string("::") >> qi::repeat(2)[ _h16_colon ] >> _ls32 )
-                         | ( -( qi::repeat(0, 3)[ _h16_colon ] >> _h16 ) >> qi::string("::") >>                _h16_colon   >> _ls32 )
-                         | ( -( qi::repeat(0, 5)[ _h16_colon ] >> _h16 ) >> qi::string("::") >>                                _ls32 )
-                         | ( -( qi::repeat(0, 4)[ _h16_colon ] >> _h16 ) >> qi::string("::") >>                                _h16  )
-                         | ( -( qi::repeat(0, 6)[ _h16_colon ] >> _h16 ) >> qi::string("::")                                         );
+        _IPv6address     %=  qi::hold[ _IPv6address_8   ]
+                           | qi::hold[ _IPv6address_0_7 ]
+                           | qi::hold[ _IPv6address_1_6 ]
+                           | qi::hold[ _IPv6address_2_5 ]
+                           | qi::hold[ _IPv6address_3_4 ]
+                           | qi::hold[ _IPv6address_4_3 ]
+                           | qi::hold[ _IPv6address_5_2 ]
+                           | qi::hold[ _IPv6address_6_1 ]
+                           | qi::hold[ _IPv6address_7_0 ];
         _IPv6address.name("uri.IPv6address");
+
+        _IPv6address_8   =                                                                                qi::repeat(6)[ _h16_colon ] >> _ls32;
+        _IPv6address_0_7 =                                                            qi::string("::") >> qi::repeat(5)[ _h16_colon ] >> _ls32;
+        _IPv6address_1_6 = -( _h16                                               ) >> qi::string("::") >> qi::repeat(4)[ _h16_colon ] >> _ls32;
+        _IPv6address_2_5 = -( _h16 >> qi::repeat(0, 1)[ qi::hold[ _colon_h16 ] ] ) >> qi::string("::") >> qi::repeat(3)[ _h16_colon ] >> _ls32;
+        _IPv6address_3_4 = -( _h16 >> qi::repeat(0, 2)[ qi::hold[ _colon_h16 ] ] ) >> qi::string("::") >> qi::repeat(2)[ _h16_colon ] >> _ls32;
+        _IPv6address_4_3 = -( _h16 >> qi::repeat(0, 3)[ qi::hold[ _colon_h16 ] ] ) >> qi::string("::") >>                _h16_colon   >> _ls32;
+        _IPv6address_5_2 = -( _h16 >> qi::repeat(0, 4)[ qi::hold[ _colon_h16 ] ] ) >> qi::string("::") >>                                _ls32;
+        _IPv6address_6_1 = -( _h16 >> qi::repeat(0, 5)[ qi::hold[ _colon_h16 ] ] ) >> qi::string("::") >>                                _h16 ;
+        _IPv6address_7_0 = -( _h16 >> qi::repeat(0, 6)[ qi::hold[ _colon_h16 ] ] ) >> qi::string("::");
 
         // h16           =            1*4   HEXDIG
         _h16             = qi::repeat(1, 4)[ascii::xdigit];
 
         // cutom
         _h16_colon       = _h16 >> ascii::char_(':');
+        _colon_h16       = ascii::char_(':') >> _h16;
 
-        // ls32          = (  h16              ":"      h16 ) /  IPv4address
-        _ls32            = ( _h16 >> qi::char_(':') >> _h16 ) | _IPv4address;
+        // ls32          =         (  h16              ":"      h16 ) /  IPv4address
+        _ls32            = qi::hold[ _h16 >> qi::char_(':') >> _h16 ] | _IPv4address;
 
         // IPv4address   =  dec-octet "." dec-octet "." dec-octet "." dec-octet
         _IPv4address     = _dec_octet >> qi::repeat(3)[qi::char_('.') >> _dec_octet];
@@ -77,19 +89,34 @@ public:
         //               / "1" 2DIGIT            ; 100-199
         //               / "2" %x30-34 DIGIT     ; 200-249
         //               / "25" %x30-35          ; 250-255
-        _dec_octet       = ( ascii::digit )
-                         | ( qi::char_('1', '9') >> ascii::digit )
-                         | ( qi::char_('1') >> ascii::digit >> ascii::digit )
-                         | ( qi::char_('2') >> qi::char_('0', '4') >> ascii::digit )
-                         | ( qi::char_('2') >> qi::char_('5') >> qi::char_('0', '5') );
+        _dec_octet       = qi::hold[ qi::char_('1') >> ascii::digit >> ascii::digit ]
+                         | qi::hold[ qi::char_('2') >> qi::char_('0', '4') >> ascii::digit ]
+                         | qi::hold[ qi::char_('2') >> qi::char_('5') >> qi::char_('0', '5') ]
+                         | qi::hold[ qi::char_('1', '9') >> ascii::digit ]
+                         | ascii::digit;
     }
     // clang-format on
 
     void enable_debug()
     {
-        BOOST_SPIRIT_DEBUG_NODE( _IPv4address );
-        BOOST_SPIRIT_DEBUG_NODE( _IPv6address );
         BOOST_SPIRIT_DEBUG_NODE( _IPvFuture );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_8 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_0_7 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_1_6 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_2_5 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_3_4 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_4_3 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_5_2 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_6_1 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv6address_7_0 );
+        BOOST_SPIRIT_DEBUG_NODE( _h16 );
+        BOOST_SPIRIT_DEBUG_NODE( _h16_colon );
+        BOOST_SPIRIT_DEBUG_NODE( _colon_h16 );
+        BOOST_SPIRIT_DEBUG_NODE( _ls32 );
+        BOOST_SPIRIT_DEBUG_NODE( _IPv4address );
+        BOOST_SPIRIT_DEBUG_NODE( _dec_octet );
+        _char.enable_debug();
     }
 
     const rule_type & v4() const { return _IPv4address; }
@@ -100,8 +127,18 @@ public:
 private:
     rule_type                         _IPvFuture;
     rule_type                         _IPv6address;
+    rule_type                         _IPv6address_8;
+    rule_type                         _IPv6address_0_7;
+    rule_type                         _IPv6address_1_6;
+    rule_type                         _IPv6address_2_5;
+    rule_type                         _IPv6address_3_4;
+    rule_type                         _IPv6address_4_3;
+    rule_type                         _IPv6address_5_2;
+    rule_type                         _IPv6address_6_1;
+    rule_type                         _IPv6address_7_0;
     rule_type                         _h16;
     rule_type                         _h16_colon;
+    rule_type                         _colon_h16;
     rule_type                         _ls32;
     rule_type                         _IPv4address;
     rule_type                         _dec_octet;
--- test/uri/parsers/rules/uri/character.test.cpp
+++ test/uri/parsers/rules/uri/character.test.cpp
@@ -3,6 +3,9 @@
 
 
 
+#include "../../test_parser.hpp"
+#include "../make_char_map.hpp"
+
 #include <uri/parsers/rules/uri/character.hpp>
 
 #include <boost/range/irange.hpp>
@@ -17,83 +20,15 @@ namespace rules {
 namespace uri {
 namespace test {
 
-using char_bool_map = std::map< char, bool >;
-char_bool_map createCharMap()
-{
-    char_bool_map result;
-    for ( auto c : boost::irange< char >( 0, 127 ) ) { result[c] = false; }
-    return result;
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-
-void enable( char_bool_map & map, char c )
-{
-    map[c] = true;
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-
-void enable( char_bool_map & map, char begin, char end )
-{
-    for ( auto c : boost::irange< char >( begin, end + 1 ) ) { enable( map, c ); }
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-
-template < typename Expr >
-void test_char_parser( const std::string & src, Expr const & expr, bool expectedResult )
-{
-    auto first = src.begin();
-    auto last  = src.end();
-
-    std::string result;
-    bool        parseResult = boost::spirit::qi::parse( first, last, expr, result );
-    BOOST_TEST( ( parseResult && ( first == last ) && ( result == src ) ) == expectedResult );
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-
-template < typename Expr >
-void test_char_parser( char src, Expr const & expr, bool parseResult )
-{
-    test_char_parser( std::string{ src }, expr, parseResult );
-}
-
-//--------------------------------------------------------------------------------------------------------------------//
-//--------------------------------------------------------------------------------------------------------------------//
-//--------------------------------------------------------------------------------------------------------------------//
 
 BOOST_AUTO_TEST_CASE( pchar )
 {
     character< std::string::const_iterator > character;
 
-    auto map = createCharMap();
-    enable( map, '0', '9' );
-    enable( map, 'a', 'z' );
-    enable( map, 'A', 'Z' );
-    enable( map, '-' );
-    enable( map, '.' );
-    enable( map, '_' );
-    enable( map, '~' );
+    auto map = rules::test::make_uri_pchar_map();
 
-    enable( map, '!' );
-    enable( map, '$' );
-    enable( map, '&' );
-    enable( map, '\'' );
-    enable( map, '(' );
-    enable( map, ')' );
-    enable( map, '*' );
-    enable( map, '+' );
-    enable( map, ',' );
-    enable( map, ';' );
-    enable( map, '=' );
-
-    enable( map, ':' );
-    enable( map, '@' );
-
-    for ( auto v : map ) { test_char_parser( v.first, character.pchar(), v.second ); }
-    test_char_parser( "%00", character.pchar(), true );
+    for ( auto v : map ) { parsers::test::test_char_parser( v.first, character.pchar(), v.second ); }
+    parsers::test::test_string_parser( "%00", character.pchar(), true );
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -102,9 +37,9 @@ BOOST_AUTO_TEST_CASE( percent_encoded )
 {
     character< std::string::const_iterator > character;
 
-    test_char_parser( "%0", character.percent_encoded(), false );
-    test_char_parser( "%00", character.percent_encoded(), true );
-    test_char_parser( "%000", character.percent_encoded(), false );
+    parsers::test::test_string_parser( "%0", character.percent_encoded(), false );
+    parsers::test::test_string_parser( "%00", character.percent_encoded(), true );
+    parsers::test::test_string_parser( "%000", character.percent_encoded(), false );
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -113,16 +48,9 @@ BOOST_AUTO_TEST_CASE( unreserved )
 {
     character< std::string::const_iterator > character;
 
-    auto map = createCharMap();
-    enable( map, '0', '9' );
-    enable( map, 'a', 'z' );
-    enable( map, 'A', 'Z' );
-    enable( map, '-' );
-    enable( map, '.' );
-    enable( map, '_' );
-    enable( map, '~' );
+    auto map = rules::test::make_uri_unreserved_map();
 
-    for ( auto v : map ) { test_char_parser( v.first, character.unreserved(), v.second ); }
+    for ( auto v : map ) { parsers::test::test_char_parser( v.first, character.unreserved(), v.second ); }
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -131,28 +59,9 @@ BOOST_AUTO_TEST_CASE( reserved )
 {
     character< std::string::const_iterator > character;
 
-    auto map = createCharMap();
-    enable( map, ':' );
-    enable( map, '/' );
-    enable( map, '?' );
-    enable( map, '#' );
-    enable( map, '[' );
-    enable( map, ']' );
-    enable( map, '@' );
+    auto map = rules::test::make_uri_reserved_map();
 
-    enable( map, '!' );
-    enable( map, '$' );
-    enable( map, '&' );
-    enable( map, '\'' );
-    enable( map, '(' );
-    enable( map, ')' );
-    enable( map, '*' );
-    enable( map, '+' );
-    enable( map, ',' );
-    enable( map, ';' );
-    enable( map, '=' );
-
-    for ( auto v : map ) { test_char_parser( v.first, character.reserved(), v.second ); }
+    for ( auto v : map ) { parsers::test::test_char_parser( v.first, character.reserved(), v.second ); }
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -161,16 +70,9 @@ BOOST_AUTO_TEST_CASE( gen_delims )
 {
     character< std::string::const_iterator > character;
 
-    auto map = createCharMap();
-    enable( map, ':' );
-    enable( map, '/' );
-    enable( map, '?' );
-    enable( map, '#' );
-    enable( map, '[' );
-    enable( map, ']' );
-    enable( map, '@' );
+    auto map = rules::test::make_uri_gen_delims_map();
 
-    for ( auto v : map ) { test_char_parser( v.first, character.gen_delims(), v.second ); }
+    for ( auto v : map ) { parsers::test::test_char_parser( v.first, character.gen_delims(), v.second ); }
 }
 
 //--------------------------------------------------------------------------------------------------------------------//
@@ -179,20 +81,9 @@ BOOST_AUTO_TEST_CASE( sub_delims )
 {
     character< std::string::const_iterator > character;
 
-    auto map = createCharMap();
-    enable( map, '!' );
-    enable( map, '$' );
-    enable( map, '&' );
-    enable( map, '\'' );
-    enable( map, '(' );
-    enable( map, ')' );
-    enable( map, '*' );
-    enable( map, '+' );
-    enable( map, ',' );
-    enable( map, ';' );
-    enable( map, '=' );
+    auto map = rules::test::make_uri_sub_delims_map();
 
-    for ( auto v : map ) { test_char_parser( v.first, character.sub_delims(), v.second ); }
+    for ( auto v : map ) { parsers::test::test_char_parser( v.first, character.sub_delims(), v.second ); }
 }
 
 } // namespace test
